ca1409d2584cef2f840f58bfc6a7a1921bf222f1
KEV040303 <kevinandresyanezmail.com>
2024-04-12 10:28:10 -0500

    Tercero

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import http from 'http'
+import { PORT } from './config.js'
+import { getUsuarios } from './controller.js'
+
+export const server = http.createServer((req, res) => {
+  const { method, url } = req
+
+  if (method === 'GET') {
+    switch (url) {
+      case '/':
+        break
+      case '/api/usuarios':
+        getUsuarios(req, res)
+        break
+      default:
+        res.writeHead(404, { 'Content-type': 'application/jason' })
+        res.end(JSON.stringify({ error: 'Not found' }))
+        break
+    }
+  }
+})
+
+server.listen(PORT, () => {
+  console.log(`Server running on port http://localhost:${PORT}`)
+})
